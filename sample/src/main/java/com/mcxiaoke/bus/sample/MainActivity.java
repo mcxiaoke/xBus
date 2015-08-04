930d2e0c126414d156a35a725f4a81a1b80ee40d
mcxiaoke <mcxiaoke@gmail.com>
2015-08-04 15:35:22 +0800

    add empty sample module

---
+++ sample/src/main/java/com/mcxiaoke/bus/sample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mcxiaoke.bus.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * User: mcxiaoke
+ * Date: 15/8/4
+ * Time: 15:31
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
